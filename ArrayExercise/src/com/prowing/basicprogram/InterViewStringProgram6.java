3f08a35bc85b22bbf32363ea797f0e07a5cfdd3e
vaibhavvyavhare45 <vaibhav.vyavhare45@gmail.com>
2022-12-24 10:10:54 +0530

    second commit

--- ArrayExercise/src/com/prowing/basicprogram/InterViewStringProgram6.java
+++ ArrayExercise/src/com/prowing/basicprogram/InterViewStringProgram6.java
@@ -5,11 +5,13 @@ public class InterViewStringProgram6 {
 		String s = "va3ib2ha5v45";
 		char[] ch = s.toCharArray();
 		for (int i = 0; i < ch.length; i++) {
-			if (Character.isAlphabetic(ch[i])) {
-
-			} else {
-				System.out.print(ch[i]);
+				while(Character.isDigit(ch[i])) {
+					System.out.println(ch[i]);
+					continue;
+				}
+				System.out.print(",");
+				
 			}
 		}
 	}
-}
+
